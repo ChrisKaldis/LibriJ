3224aa30049cdb5fd8f582e151832c472fb8f2b3
Christos Kaldis <kaldischristos@gmail.com>
2026-01-28 19:15:37 +0200

    Add repositories

---
+++ src/main/java/com/kaldis/chris/librij/data/jpa/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package com.kaldis.chris.librij.data.jpa.repository;
+
+import com.kaldis.chris.librij.domain.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, UUID> {
+}
